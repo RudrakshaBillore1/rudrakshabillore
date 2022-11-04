d95a9e5da2166e62ac7e41c4bef834e0476ec2d5
“RudrakshaBillore1” <rudrakshabillore@gmail.com>
2022-11-05 00:51:01 +0530

    first commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app
     });
     server.listen(3000, (err) => {
       if (err) throw err;
-      console.log("> Ready on http://");
+      console.log("> Ready on http://localhost:3000/");
     });
   })
   .catch((ex) => {
