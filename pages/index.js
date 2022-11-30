935c7e046aa996c96b01d718e829e2fdd6c2e04b
Rudraksha Billore <102648499+RudrakshaBillore1@users.noreply.github.com>
2022-11-30 22:19:41 +0530

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
 import FavouriteProjects from "../components/FavouriteProjects";
